745a6c96ac6988319a29f8057f3528b3d0dd657d
Mila <lu.pshenichnikova@gmail.com>
2023-03-13 17:31:17 +0300

    New

---
+++ src/test/java/ru/netology/GameTest.java
@@ -0,0 +1,100 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+    @Test
+    public void testFirstPlayer() {
+        Game game = new Game();
+        Player ivan = new Player(1, "Иван", 100);
+        Player fedor = new Player(2, "Федор", 90);
+
+        game.register(ivan);
+        game.register(fedor);
+        int expected = 1;
+        int actual = game.round("Иван", "Федор");
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testSecondPlayer() {
+        Game game = new Game();
+        Player ivan = new Player(1, "Иван", 70);
+        Player fedor = new Player(2, "Федор", 190);
+
+        game.register(ivan);
+        game.register(fedor);
+        int expected = 2;
+        int actual = game.round("Иван", "Федор");
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testEqualsPlayer() {
+        Game game = new Game();
+        Player ivan = new Player(1, "Иван", 70);
+        Player fedor = new Player(2, "Федор", 70);
+
+        game.register(ivan);
+        game.register(fedor);
+        int expected = 0;
+        int actual = game.round("Иван", "Федор");
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testNotFaundFistPlayer() {
+        Game game = new Game();
+        Player ivan = new Player(1, "Иван", 100);
+        Player fedor = new Player(2, "Федор", 70);
+
+        game.register(ivan);
+        game.register(fedor);
+
+        Assertions.assertThrows(NotRegisteredException.class,
+                () -> {
+                    game.round("Илья", "Федор");
+                });
+
+    }
+
+    @Test
+    public void testNotFaundSecondPlayer() {
+        Game game = new Game();
+        Player ivan = new Player(1, "Иван", 70);
+        Player fedor = new Player(2, "Федор", 120);
+
+        game.register(ivan);
+        game.register(fedor);
+        int expected = 1;
+        int actual = game.round("Иван", "Федор");
+
+        Assertions.assertThrows(expected, actual);
+
+    }
+
+    @Test
+    public void testNotFaundAllPlayers() {
+        Game game = new Game();
+        Player ivan = new Player(1, "Иван", 70);
+        Player fedor = new Player(2, "Федор", 100);
+
+        game.register(ivan);
+        game.register(fedor);
+        int expected = 0;
+        int actual = game.round("Илья", "Игорь");
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+}
